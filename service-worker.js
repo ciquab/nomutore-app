d34ba63003208a8ce6b33cf3f0eadad2d8ce79ce
ciquab <ouioa8.15pc@gmail.com>
2026-01-09 00:33:58 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nomutore-v66'; // 更新時はここを変更
+const CACHE_NAME = 'nomutore-v67'; // 更新時はここを変更
 
 // 1. アプリ本体のファイル（確実にキャッシュする）
 // ※外部URL(CDN)はここには含めないでください
@@ -79,3 +79,4 @@ self.addEventListener('fetch', (event) => {
     );
 
 });
+
